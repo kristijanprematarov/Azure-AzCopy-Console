113cd876c4d9954d45dab1f487608360cbb334ed
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2023-02-17 21:46:23 +0100

    Add project files.

---
+++ StorageCopy/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+
+var connectionString = "";
+
+BlobServiceClient? blobServiceClient = new BlobServiceClient(connectionString);
+
+var source = "firstcontainer";
+var destination = "secondcontainer";
+var sourcefile = "Book1.pdf";
+var destfile = "Book1-COPY.pdf";
+
+var sourceClient = new BlockBlobClient(connectionString, source, sourcefile);
+var destinationClient = new BlockBlobClient(connectionString, destination, destfile);
+
+await destinationClient.StartCopyFromUriAsync(sourceClient.Uri);
